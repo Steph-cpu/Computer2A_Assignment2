3b5c0078be73d582f28742dc5d074e42859d4747
Ruizhe Yang <18310281800@163.com>
2025-11-29 12:08:15 +0800

    Replace test01 with test02 in main function

--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
-
 #include "ManchesterBaby.h"
 using namespace std;
-void test01() {
-    ManchesterBaby mb;
-    mb.getStatus();
-    string file="/Users/yangruizhe/Desktop/BabyTest1-MC.txt";
-    bool res=mb.getCode(file);
-    cout<<res<<endl;
-    mb.getStatus();
+void test02() {
+    ManchesterBaby mb1;
+    string file="/Users/yangruizhe/Desktop/BabyTest1-MC.txt";   //换成你自己的文件路径
+    mb1.getCode(file);
+    mb1.cycle();
+    cout<<"Test02 Accumulator: "<<mb1.getAccumulator()<<endl;   //结果应该直接输出1646
 }
-
 int main() {
-    test01();
+    test02();
     return 0;
+
 }
