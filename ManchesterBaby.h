7e880d5d2ee074754fe074b264ef3d8abcfe4c08
Ruizhe Yang <18310281800@163.com>
2025-12-05 15:53:09 +0800

    Refactor ManchesterBaby.h structure and variable names

--- ManchesterBaby.h
+++ ManchesterBaby.h
@@ -37,12 +37,13 @@ private:
         int32_t address;  // 8-bit address
     };
     
-    instruction currInst; // current instruction ,当前指令
-    uint8_t memorySize = 32; // memory size ,存储器大小 (扩展为 64 行)
-    vector<int32_t> memory; // 32-bit memory ,主存储器
-    int32_t accumulator;    // 32-bit accumulator ,累加器
-    uint8_t pi;           // program instruction register ,程序指令寄存器
-    bool isTerminated = false; // flag to indicate if the program has terminated ,程序是否结束的标志
+    instruction currInst; // current instruction
+    uint8_t memorySize = 32; // memory size(32-64)
+    vector<int32_t> memory; // 32-bit memory
+    int32_t accumulator;    // 32-bit accumulator
+    uint8_t pc;           // program counter register
+    int32_t pi;              // program instruction register
+    bool isTerminated = false; // flag to indicate if the program has terminated
     int32_t toInt(const string& binaryStr); // convert binary string to int32_t
     string toBinary(int32_t value); // convert int32_t to binary string
 
